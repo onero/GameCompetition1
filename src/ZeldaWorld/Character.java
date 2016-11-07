5b674c608c0ded0fda8ff1bce389a0c7650b5c7e
Adam Hansen <adamlars90@gmail.com>
2016-11-07 12:29:31 +0100

    Updated structure
    Added moreabstractions

--- src/ZeldaWorld/Character.java
+++ src/ZeldaWorld/Character.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Guardian here.
+ * Write a description of class Character here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Guardian extends Character
+public class Character extends ScrollActor
 {
     /**
-     * Act - do whatever the Guardian wants to do. This method is called whenever
+     * Act - do whatever the Character wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
--- src/ZeldaWorld/Enemy.java
+++ src/ZeldaWorld/Enemy.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Enemy extends ScrollActor
+public class Enemy extends Character
 {
     /**
      * Act - do whatever the Enemy wants to do. This method is called whenever
