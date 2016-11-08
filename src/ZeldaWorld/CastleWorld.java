330d64b9d2d0b156d7bd4e6cbd6d4f55ac87f657
IAML00X <cronoluke@gmail.com>
2016-11-08 11:54:13 +0100

    CastleWorld will now populate with mobs

--- src/ZeldaWorld/CastleWorld.java
+++ src/ZeldaWorld/CastleWorld.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.*;
 /**
  * Write a description of class CastleWorld here.
  * 
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class CastleWorld extends ScrollWorld
 {
+    public static final int GAME_WIDTH = 2000;
+    public static final int GAME_HEIGHT = 2000;    
 
     /**
      * Constructor for objects of class CastleWorld.
@@ -16,6 +18,13 @@ public class CastleWorld extends ScrollWorld
     public CastleWorld()
     {
         super(600, 400, 1, ZeldaWorld.GAME_WIDTH, ZeldaWorld.GAME_HEIGHT);
+        Random rand = new Random();
         //TODO ALH: Add enemies and objects!
+                for (int i = 0; i < 10; i++) {
+            addObject(new Pillar(), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
+            addObject(new Rock(), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
+            addObject(new Rat(2,2), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
+            addObject(new Knight(20), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
+        }
     }
 }
