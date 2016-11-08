7a7eb0f6cfebc81ce913b5745e6f3e66fa2615ce
Adam Hansen <adamlars90@gmail.com>
2016-11-08 11:43:24 +0100

    Refactored creation intoseperate methods in the constructor of ZeldaWorld

--- src/ZeldaWorld/ZeldaWorld.java
+++ src/ZeldaWorld/ZeldaWorld.java
@@ -11,40 +11,57 @@ public class ZeldaWorld extends ScrollWorld
     public static final int GAME_HEIGHT = 2000;
     public static DungeonWorld dungeonWorld;
     public static CastleWorld castleWorld;
+    private Random rand;
     /**
      * Constructor for objects of class DemoWorld.
      */
     public ZeldaWorld()
     {
         super(600, 600, 1, GAME_WIDTH, GAME_HEIGHT);
-        Random rand = new Random();
+        rand = new Random();
         //Add worlds
         dungeonWorld = new DungeonWorld();
         castleWorld = new CastleWorld();
-        //Add objects
-        for (int i = 0; i < 10; i++) {
-            addObject(new Rock(), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
-            addObject(new Wall(), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
-            addObject(new Tree(), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
-            addObject(new WallWithEntrance(), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
-        }
-        addObject(new Dungeon(), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
+
+        createObjects();
 
         addCameraFollower(new Link(10, 5), 0, 0);
 
-        //addObject(new FPS(), 85, 15); // FPS isn't a subclass of
-        // ScrollActor, so it will looklike it's a camera 
+        createBosses();
+
+        createEnemies();
         
-        //Add boses
+    }
+
+    /**
+     *Create the objects
+     */
+    private void createObjects() {
+        for (int i = 0; i < 10; i++) {
+            addObject(new Rock(), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
+            addObject(new Wall(), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
+            addObject(new WallWithEntrance(), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
+        }
+        addObject(new Dungeon(), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
+    }
+
+    /**
+     *Create the bosses
+     */
+    private void createBosses() {
         addObject(new PlayerInfo(), 85, 15);
         addObject(new StigEvil(30, 5, "wand", 5), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT)); 
         addObject(new JeppeTheForker(20, 6, "trident", 6),rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
         addObject(new Peter(15, 4, "sword", 4), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
+    }
 
-        //Add Enemies
+    /**
+     *Create the Enemies
+     */
+    private void createEnemies() {
         for (int i = 0; i < 10; i++) {
             addObject(new Rat(5, 1), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
-            addObject(new Slime(), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
+            addObject(new Slime(8,3), rand.nextInt(GAME_WIDTH), rand.nextInt(GAME_HEIGHT));
         }
     }
 }
