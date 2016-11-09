d66ae0d07c2d2eb572e6940e803697f3921d5b88
IAML00X <cronoluke@gmail.com>
2016-11-09 11:29:43 +0100

    Added musicplayer to the project
    4 tracks added so far

---
+++ src/ZeldaWorld/MusicPlayer.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
+/**
+ * Write a description of class MusicPlayer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MusicPlayer extends Actor
+{
+    private final String DESTINATION_PREFIX = "sounds/";
+    private ArrayList<String> musicArray;
+    
+    public MusicPlayer()
+    {
+        musicArray = new ArrayList();
+        
+        musicArray.add(DESTINATION_PREFIX + "Castle" + ".mp3");
+        musicArray.add(DESTINATION_PREFIX + "Dungeon" + ".mp3");
+        musicArray.add(DESTINATION_PREFIX + "ZeldaOverWorld" + ".mp3");
+        musicArray.add(DESTINATION_PREFIX + "TitleMusic" + ".mp3");
+    }
+        
+        
+    
+    
+    /**
+     * Act - do whatever the MusicPlayer wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }
+}
