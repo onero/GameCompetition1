be4b15fb9e810b7d82a80d803654f9e79f225f0d
Adam Hansen <adamlars90@gmail.com>
2016-11-10 15:59:06 +0100

    Refactored more methods

--- src/ZeldaWorld/ZeldaWorld.java
+++ src/ZeldaWorld/ZeldaWorld.java
@@ -7,22 +7,15 @@ import java.util.*;
  */
 public class ZeldaWorld extends MasterWorld
 {
-    public static final int GAME_WIDTH = 2000;
-    public static final int GAME_HEIGHT = 2000;
-    public static final int GAME_AREA = 1000;
-    public static final int GAME_AREA_MIN = 250;
-    public static final int PLAYABLE_AREA = 600;
     private boolean dungeonEntranceCreated;
     private Random rand;
     private static Character link;
-    public static DungeonWorld dungeonWorld;
-
     /**
      * Constructor for objects of class DemoWorld.
      */
     public ZeldaWorld()
     {
-        super(PLAYABLE_AREA, PLAYABLE_AREA, 1, GAME_WIDTH, GAME_HEIGHT);
+        super(MasterWorld.PLAYABLE_AREA, MasterWorld.PLAYABLE_AREA, 1, MasterWorld.GAME_HEIGHT, MasterWorld.GAME_WIDTH);
         rand = new Random();
         dungeonEntranceCreated = false;
         //Create objects
@@ -48,30 +41,30 @@ public class ZeldaWorld extends MasterWorld
      */
     private void createObjects() {
         //Top wall
-        for(int i = GAME_AREA_MIN; i < GAME_WIDTH - GAME_AREA_MIN; i += 215)
+        for(int i = MasterWorld.GAME_AREA_MIN; i < MasterWorld.GAME_WIDTH - MasterWorld.GAME_AREA_MIN; i += 215)
         {
-            addObject(new Wall(), i , GAME_AREA_MIN - 60);
+            addObject(new Wall(), i , MasterWorld.GAME_AREA_MIN - 60);
         }
-        addObject(new WallWithEntrance(), 500, GAME_AREA_MIN - 60);
+        addObject(new WallWithEntrance(), 500, MasterWorld.GAME_AREA_MIN - 60);
         //Left side wall
-        for(int i = 250; i < GAME_WIDTH - GAME_AREA_MIN; i += 150)
+        for(int i = 250; i < MasterWorld.GAME_WIDTH - MasterWorld.GAME_AREA_MIN; i += 150)
         {
             Wall sideWall = new Wall();
             addObject(sideWall, 100, i);
             sideWall.setRotation(sideWall.getRotation() - 90);
         }
         //Buttom side wall
-        for(int i = GAME_AREA_MIN; i < GAME_WIDTH - GAME_AREA_MIN; i += 150)
+        for(int i = MasterWorld.GAME_AREA_MIN; i < MasterWorld.GAME_WIDTH - MasterWorld.GAME_AREA_MIN; i += 150)
         {
             Wall sideWall = new Wall();
-            addObject(sideWall, i , GAME_WIDTH - GAME_AREA_MIN);
+            addObject(sideWall, i , MasterWorld.GAME_WIDTH - MasterWorld.GAME_AREA_MIN);
             sideWall.setRotation(sideWall.getRotation() - 180);
         }
         //Right side wall
-        for(int i = 350; i < GAME_WIDTH - GAME_AREA_MIN; i += 150)
+        for(int i = 350; i < MasterWorld.GAME_WIDTH - MasterWorld.GAME_AREA_MIN; i += 150)
         {
             Wall sideWall = new Wall();
-            addObject(sideWall, GAME_WIDTH - 180, i);
+            addObject(sideWall, MasterWorld.GAME_WIDTH - 180, i);
             sideWall.setRotation(sideWall.getRotation() + 90);
         }
         //Add rocks
@@ -79,26 +72,26 @@ public class ZeldaWorld extends MasterWorld
         addObject(new Rock(),1400, 700);
         addObject(new Rock(),1050, 1000);
         // Trees
-        for(int i = 940 ; i < GAME_WIDTH - 300 ; i += 200)
+        for(int i = 940 ; i < MasterWorld.GAME_WIDTH - 300 ; i += 200)
         { 
-            addObject(new Tree(), GAME_WIDTH - 1500, i);
-            addObject(new Tree(), GAME_WIDTH - 1250, i);
+            addObject(new Tree(), MasterWorld.GAME_WIDTH - 1500, i);
+            addObject(new Tree(), MasterWorld.GAME_WIDTH - 1250, i);
         }
-        for(int i = 1140 ; i < GAME_WIDTH - 300 ; i += 200)
+        for(int i = 1140 ; i < MasterWorld.GAME_WIDTH - 300 ; i += 200)
         { 
-            addObject(new Tree(), GAME_WIDTH - 1000, i);
-            addObject(new Tree(), GAME_WIDTH - 750, i);
-            addObject(new Tree(), GAME_WIDTH - 500, i);
+            addObject(new Tree(), MasterWorld.GAME_WIDTH - 1000, i);
+            addObject(new Tree(), MasterWorld.GAME_WIDTH - 750, i);
+            addObject(new Tree(), MasterWorld.GAME_WIDTH - 500, i);
             
         }
 
         for(int i = 700 ; i < 1600; i += 215)
         { 
-            addObject(new Tree(), i , GAME_AREA_MIN + 100);
+            addObject(new Tree(), i , MasterWorld.GAME_AREA_MIN + 100);
         }
-        for(int i = 500 ; i < GAME_WIDTH - 1000 ; i += 200)
+        for(int i = 500 ; i < MasterWorld.GAME_WIDTH - 1000 ; i += 200)
         {
-            addObject(new Tree(), GAME_WIDTH - 350, i);
+            addObject(new Tree(), MasterWorld.GAME_WIDTH - 350, i);
         }
     }
 
